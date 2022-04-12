0b0d8f9cfd35ef0ca747cb31c9f57662a701cef1
fortune <omorodion.fortune@gmail.com>
2022-04-12 21:13:02 +0100

    installed react-tinder-card to project

--- tinder-app/src/App.js
+++ tinder-app/src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Link, Route } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Header";
+import TinderCards from "./components/TinderCards";
 
 const App = () => {
   return (
-    <div>
+    <div className="App">
       <Header />
+      <Router>
+        <Routes>
+          <Route path="/" element={<TinderCards />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 };
