889f238fcb58b5d5d3dc6d45a4fb4494a8580b59
fortune <omorodion.fortune@gmail.com>
2022-04-16 21:29:17 +0100

    created th swipeButton component

---
+++ tinder-app/src/components/SwipeButtons.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const SwipeButtons = () => {
+  return (
+    <div className="swipeButton">
+      <h1>Button goes here!</h1>
+      <button className="mini ui icon button">
+        <i class="x icon"></i>
+      </button>
+      <button className="mini ui icon button">
+        <i class="undo icon"></i>
+      </button>
+      <button className="mini ui icon button">
+        <i class="star icon"></i>
+      </button>
+      <button className="mini ui icon button">
+        <i class="heart icon"></i>
+      </button>
+      <button className="mini ui icon button">
+        <i class="bolt icon"></i>
+      </button>
+    </div>
+  );
+};
+
+export default SwipeButtons;
